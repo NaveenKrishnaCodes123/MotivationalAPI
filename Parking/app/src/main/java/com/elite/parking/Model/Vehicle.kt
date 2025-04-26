de3644a7276e6d35b80c06d5fe1414e96f6b66ec
Naveen Krishna <naveenkrishnacodes@gmail.com>
2025-04-26 16:05:45 +0530

    new project

--- Parking/app/src/main/java/com/elite/parking/Model/Vehicle.kt
+++
@@ -1,9 +0,0 @@
-package com.elite.parking.Model
-
-data class Vehicle(
-    val vehicleNumber: String,
-    val make: String,
-    val model: String,
-    val hookNumber: String,
-    val status: String
-)
